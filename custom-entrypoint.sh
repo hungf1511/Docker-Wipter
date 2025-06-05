2a81ce5ac5ea42f89fb5881c39ea408f32a7f185
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-06 05:00:04 +0800

    update

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -m
 
+MASKED_PASSWORD=$(printf '***%.0s' $(seq ${#WIPTER_PASSWORD}
+
+echo " "
+echo "=== === === === === === === ==="
 echo "Executing custom entrypoint ..."
+echo "=== === === === === === === ==="
+echo " "
 
 eval "$(dbus-launch --sh-syntax)"
 echo "$WIPTER_PASSWORD" | gnome-keyring-daemon --unlock --replace
@@ -10,16 +16,27 @@ setup_wipter() {
   FLAG_FILE="/root/.config/wipter.setup_done"
 
   if [ -f "$FLAG_FILE" ]; then
+    echo " "
+    echo "=== === === === === === === === === === === === === ==="
     echo "Wipter setup is already done; skipping initialization."
+    echo "=== === === === === === === === === === === === === ==="
+    echo " "
     return 0
   fi
 
   if [ -z "$WIPTER_EMAIL" ] || [ -z "$WIPTER_PASSWORD" ]; then
+    echo " "
+    echo "=== === === === === === === === === === === === === ==="
     echo "WIPTER_EMAIL or WIPTER_PASSWORD is not set or is blank."
+    echo "=== === === === === === === === === === === === === ==="
+    echo " "
     return 0
   fi
-
+  echo " "
+  echo "=== === === === === === === === === === === ==="
   echo "Found necessary login details. Trying now ..."
+  echo "=== === === === === === === === === === === ==="
+  echo " "
   
   local WIPTER_WIN=""
   local attempts=0
@@ -34,7 +51,11 @@ setup_wipter() {
   done
 
   if [ -z "$WIPTER_WIN" ]; then
+    echo " "
+    echo "=== === === === === === === === === === === === ==="
     echo "Wipter window was not found after waiting. Exiting."
+    echo "=== === === === === === === === === === === === ==="
+    echo " "
     return 0
   fi
 
@@ -46,23 +67,39 @@ setup_wipter() {
   sleep 3
   xte "key Tab"
   sleep 3
+  echo " "
+  echo "=== === === === === === === === === === === === ==="
+  echo "Typing EMAIL = $WIPTER_EMAIL"
+  echo "=== === === === === === === === === === === === ==="
+  echo " "
   xte "str $WIPTER_EMAIL"
   sleep 3
   xte "key Tab"
   sleep 3
+  echo " "
+  echo "=== === === === === === === === === === === === ==="
+  echo "Typing PASSWORD = $MASKED_PASSWORD"
+  echo "=== === === === === === === === === === === === ==="
+  echo " "
   xte "str $WIPTER_PASSWORD"
   sleep 3
   xte "key Return"
 
+  echo " "
+  echo "=== === === === === === === === === === === === ==="
   echo "Wipter setup complete."
-
+  echo "=== === === === === === === === === === === === ==="
+  echo " "
   mkdir -p "$(dirname "$FLAG_FILE")"
   touch "$FLAG_FILE"
   return 0
 }
 
-
+echo " "
+echo "=== === === === ==="
 echo "Starting Wipter ..."
+echo "=== === === === ==="
+echo " "
 /opt/Wipter/wipter-app &
 sleep 5
 setup_wipter
